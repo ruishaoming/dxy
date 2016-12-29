1e394cc3a1b707586449ee5a973136b38a474755
ruijinglin <574675213@qq.com>
2016-12-29 09:05:33 +0800

    主界面更改

--- app/src/main/java/com/dxy/happy/activity/MainActivity.java
+++ app/src/main/java/com/dxy/happy/activity/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         initViews();
         initViewPager();
+        //
     }
 
     //初始化ViewPager
