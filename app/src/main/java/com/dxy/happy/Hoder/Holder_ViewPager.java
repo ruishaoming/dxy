16dfa1ab98f40e1294aa634fb2951f8b554a7446
ruijinglin <574675213@qq.com>
2017-01-03 20:57:01 +0800

    HomeFragment冲突更改

--- app/src/main/java/com/dxy/happy/Hoder/Holder_ViewPager.java
+++ app/src/main/java/com/dxy/happy/Hoder/Holder_ViewPager.java
@@ -2,14 +2,20 @@ package com.dxy.happy.Hoder;
 
 import android.content.Context;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.dxy.happy.R;
-import com.dxy.happy.adapter.Home_RecycleViewAdapter;
 import com.dxy.happy.adapter.Home_ViewPagerAdapter;
 import com.dxy.happy.bean.Fragment_ViewPagerBean;
+import com.google.gson.Gson;
 
-import java.util.List;
+import static com.dxy.happy.R.mipmap.zaker_content_praise;
+import static com.dxy.happy.R.mipmap.zaker_content_praise_press;
+
 
 /**
  * Created by 韩永光
@@ -17,17 +23,101 @@ import java.util.List;
  */
 public class Holder_ViewPager extends BaseHolder {
 
-    public ViewPager home_fragment_viewpager;
+    private ViewPager home_fragment_viewpager;
+    private LinearLayout home_fragment_ll;
+    private ImageView img;
+
 
     public Holder_ViewPager(View itemView) {
         super(itemView);
         home_fragment_viewpager = (ViewPager) itemView.findViewById(R.id.home_fragment_viewpager);
+        home_fragment_ll = (LinearLayout) itemView.findViewById(R.id.home_fragment_ll);
     }
 
     @Override
-    public void getHolder(Context context, Object o) {
-        List<Fragment_ViewPagerBean.DataBean> vpList = (List<Fragment_ViewPagerBean.DataBean>) o;
-        Home_ViewPagerAdapter adapter=new Home_ViewPagerAdapter(context,vpList);
+    public void getHolder(final Context context, Object o) {
+        String o1 = (String) o;
+        Fragment_ViewPagerBean fragment_viewPagerBean = new Gson().fromJson(o1, Fragment_ViewPagerBean.class);
+        Home_ViewPagerAdapter adapter = new Home_ViewPagerAdapter(context, fragment_viewPagerBean.getData());
+        //设置pager间 间距
+        home_fragment_viewpager.setPageMargin(10);
+        //设置缓存的页面数量
+        home_fragment_viewpager.setOffscreenPageLimit(3);
+
         home_fragment_viewpager.setAdapter(adapter);
+        //设置默认显示页
+        home_fragment_viewpager.setCurrentItem(1);
+        //viewpager特效设置
+        setViewPagerAnimtion();
+        //小点设置
+        home_fragment_ll.removeAllViews();
+        for (int i = 0; i < 5; i++) {
+
+            img = new ImageView(context);
+            if (i == 0) {
+                img.setImageResource(zaker_content_praise_press);
+            } else {
+                img.setImageResource(zaker_content_praise);
+            }
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(0, 1, 0, 1);
+            home_fragment_ll.addView(img, layoutParams);
+        }
+        home_fragment_viewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < home_fragment_ll.getChildCount(); i++) {
+                    ImageView childAt = (ImageView) home_fragment_ll.getChildAt(i);
+                    if (i == position % 5) {
+                        childAt.setImageResource(zaker_content_praise_press);
+
+                    } else {
+                        childAt.setImageResource(zaker_content_praise);
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
+
+    private void setViewPagerAnimtion() {
+        home_fragment_viewpager.setPageTransformer(false, new ViewPager.PageTransformer() {
+            private static final float MIN_SCALE = 0.90f;
+            private static final float MIN_ALPHA = 0.5f;
+
+            @Override
+            public void transformPage(View page, float position) {
+                if (position < -1 || position > 1) {
+                    page.setAlpha(MIN_ALPHA);
+                    page.setScaleX(MIN_SCALE);
+                    page.setScaleY(MIN_SCALE);
+                } else if (position <= 1) { // [-1,1]
+                    float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                    if (position < 0) {
+                        float scaleX = 1 + 0.3f * position;
+                        page.setScaleX(scaleX);
+                        page.setScaleY(scaleX);
+                    } else {
+                        float scaleX = 1 - 0.3f * position;
+                        page.setScaleX(scaleX);
+                        page.setScaleY(scaleX);
+                    }
+                    page.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+                }
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/dxy/happy/fragment/Home_Fragment.java
+++ app/src/main/java/com/dxy/happy/fragment/Home_Fragment.java
@@ -1,17 +1,23 @@
 package com.dxy.happy.fragment;
 
+import android.content.Intent;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
 
 import com.dxy.happy.R;
+import com.dxy.happy.activity.MediaActivity;
 import com.dxy.happy.adapter.Home_RecycleViewAdapter;
+import com.dxy.happy.app.XnlApplication;
 import com.dxy.happy.base.BaseData;
 import com.dxy.happy.base.BaseFragment;
 import com.dxy.happy.utils.CommonUtils;
 import com.dxy.happy.utils.URLUtils;
 import com.dxy.happy.view.ShowingPage;
+import com.zhy.autolayout.AutoLinearLayout;
 
 import java.util.ArrayList;
 
@@ -21,25 +27,17 @@ import java.util.ArrayList;
  * on 2016/12/28 11:58.
  */
 
-public class Home_Fragment extends BaseFragment  implements View.OnClickListener {
+public class Home_Fragment extends BaseFragment implements View.OnClickListener{
     private MyBaseData myBaseData;
     private RecyclerView home_fragment_recycleview;
     private SwipeRefreshLayout swipe_ly;
     private Home_RecycleViewAdapter adapter;
+    private AutoLinearLayout media;
+    private TextView media_title;
     ArrayList<String> urlList = new ArrayList<>();
     String url[] = {URLUtils.url_viewPager, URLUtils.url_festival, URLUtils.url_loveCommunity_alone
             , URLUtils.url_know, URLUtils.url_loveGas};
 
-    private AutoLinearLayout media;
-    private TextView media_title;
-
-    @Override
-    protected void onLoad() {
-        Home_Fragment.this.showCurrentPage(ShowingPage.StateType.STATE_LOAD_SUCCESS);
-
-        //请求数据
-        initGetData();
-    }
 
     @Override
     protected View createSuccessView() {
@@ -47,6 +45,13 @@ public class Home_Fragment extends BaseFragment  implements View.OnClickListener
         //初始化控件
         initView(view);
         return view;
+    }
+
+    @Override
+    protected void onLoad() {
+        Home_Fragment.this.showCurrentPage(ShowingPage.StateType.STATE_LOAD_SUCCESS);
+        //SwipeRefreshLayout刷新加载
+        initSwipeRefreshLayout();
     }
 
     @Override
@@ -60,33 +65,6 @@ public class Home_Fragment extends BaseFragment  implements View.OnClickListener
         } else {
             media.setVisibility(View.GONE);
         }
-    }
-
-    @Override
-    protected void onLoad() {
-        Home_Fragment.this.showCurrentPage(ShowingPage.StateType.STATE_LOAD_SUCCESS);
-        //SwipeRefreshLayout刷新加载
-        initSwipeRefreshLayout();
-
-    }
-
-    private void initView(View view) {
-        home_fragment_recycleview = (RecyclerView) view.findViewById(R.id.home_fragment_recycleview);
-        swipe_ly = (SwipeRefreshLayout) view.findViewById(R.id.swipe_ly);
-        media = (AutoLinearLayout) view.findViewById(R.id.home_fragment_media);
-        media_title = (TextView) view.findViewById(R.id.home_fragment_media_title);
-        media.setOnClickListener(this);
-    }
-
-    private void initGetData() {
-        new BaseData() {
-            @Override
-            public void setResultData(String reresponse) {
-                Fragment_ViewPagerBean fragment_viewPagerBean = new Gson().fromJson(reresponse, Fragment_ViewPagerBean.class);
-                //展示数据
-                initRecycleView(fragment_viewPagerBean);
-            }
-        }.getData(URLUtils.url_viewPager, BaseData.NOTIME);
     }
 
     private void initSwipeRefreshLayout() {
@@ -143,7 +121,6 @@ public class Home_Fragment extends BaseFragment  implements View.OnClickListener
                 break;
         }
     }
-
 
     class MyBaseData extends BaseData {
 
