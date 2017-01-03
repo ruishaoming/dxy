41adb22cd466334a5e5eb8001b577815d5802c3d
ruijinglin <574675213@qq.com>
2017-01-03 20:32:27 +0800

    调用系统相机、相册2

--- app/src/main/java/com/dxy/happy/Hoder/Holder_Know.java
+++ app/src/main/java/com/dxy/happy/Hoder/Holder_Know.java
@@ -2,12 +2,6 @@ package com.dxy.happy.Hoder;
 
 import android.content.Context;
 import android.view.View;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-import com.dxy.happy.R;
-import com.dxy.happy.bean.Fragment_KnowBean;
-import com.google.gson.Gson;
 
 import com.dxy.happy.Hoder.BaseHolder;
 
@@ -16,24 +10,12 @@ import com.dxy.happy.Hoder.BaseHolder;
  * on 2016/12/28 22:12.
  */
 public class Holder_Know extends BaseHolder {
-
-    private final ImageView img_know;
-
     public Holder_Know(View itemView) {
         super(itemView);
-        img_know = (ImageView) itemView.findViewById(R.id.img_know);
     }
 
     @Override
     public void getHolder(Context context, Object o) {
-        String o1 = (String) o;
-        Fragment_KnowBean fragment_knowBean = new Gson().fromJson(o1, Fragment_KnowBean.class);
-        Glide.with(context).load(fragment_knowBean.getData().get(0).getImg()).into(img_know);
-        img_know.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
     }
 }
--- app/src/main/java/com/dxy/happy/Hoder/Holder_Love_Gas.java
+++ app/src/main/java/com/dxy/happy/Hoder/Holder_Love_Gas.java
@@ -2,12 +2,6 @@ package com.dxy.happy.Hoder;
 
 import android.content.Context;
 import android.view.View;
-import android.widget.ListView;
-
-import com.dxy.happy.R;
-import com.dxy.happy.adapter.Home_lg_ListViewAdapter;
-import com.dxy.happy.bean.Fragment_LoveGasBean;
-import com.google.gson.Gson;
 
 import com.dxy.happy.Hoder.BaseHolder;
 
@@ -16,27 +10,12 @@ import com.dxy.happy.Hoder.BaseHolder;
  * on 2016/12/28 22:12.
  */
 public class Holder_Love_Gas extends BaseHolder {
-
-    private final ListView lv1_love_gas;
-    private Home_lg_ListViewAdapter adapter;
-
     public Holder_Love_Gas(View itemView) {
         super(itemView);
-
-        lv1_love_gas = (ListView) itemView.findViewById(R.id.lv1_love_gas);
     }
 
     @Override
     public void getHolder(Context context, Object o) {
-        String o1 = (String) o;
-        Fragment_LoveGasBean fragment_loveGasBean = new Gson().fromJson(o1, Fragment_LoveGasBean.class);
-        if (adapter==null){
-            adapter = new Home_lg_ListViewAdapter(context,fragment_loveGasBean);
-            lv1_love_gas.setAdapter(adapter);
-        }else {
-            adapter.notifyDataSetChanged();
-        }
-
 
     }
 }
